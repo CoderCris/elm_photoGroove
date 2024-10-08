b162c133687dafcfe40f8b08b406ad41c501b366
CoderCris <crisamer.ortega@gmail.com>
2024-10-08 20:14:40 +0100

    update imgs and model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,6 +1,6 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (..)
+import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 urlPrefix = "http://elm-in-action.com/"
@@ -8,22 +8,21 @@ urlPrefix = "http://elm-in-action.com/"
 view model =
     div [ class "content"]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] []
+        , div [ id "thumbnails" ] (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+        , img [class "large", src (urlPrefix ++ model.selectedUrl)] []
         ]
 
 viewThumbnail selectedUrl thumb =
-    if selectedUrl == thumb.url then
-        img [ src (urlPrefix ++ thumb.url), class "selected" ] []
-    else
-        img [ src (urlPrefix ++ thumb.url) ] []
+    img [ src (urlPrefix ++ thumb.url), classList [ ( "selected", selectedUrl == thumb.url ) ] ] []
     
 initialModel =
-     {photos =
+    {photos =
         [{ url = "1.jpeg" }
         , { url = "2.jpeg" }
         , { url = "3.jpeg" }
         ]
-    , selectedUrl = "1.jpeg"
+    , selectedUrl = "2.jpeg"
     }
+
 main =
     view initialModel
